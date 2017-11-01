85b35b6914cc57c6217774c486dc49791c0ad05c
erose357 <erose357@gmail.com>
2017-11-01 10:45:25 -0600

    Working through refactor, need to figure out how to send total count of stores to the view, not just the initial 10 returned

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,25 @@
 class SearchController < ApplicationController
   def index
-    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,longName,city,distance,phone&pageSize=10&page=1&apiKey=#{ENV["api_key"]}")
+    @stores = BestBuyService.new(params[:q]).find_stores
+  end
+end
 
-    raw_stores = JSON.parse(response.body, symbolize_names: true)[:stores]
-    
-    @stores = raw_stores.map do |store|
-      Store.new(store)
+class BestBuyService
+  def initialize(zip)
+    @zip = zip
+  end
+
+  def find_stores
+    url.map do |store|
+       Store.new(store)
     end
   end
+
+  private
+    attr_reader :zip
+    
+    def url
+    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=storeId,storeType,longName,city,distance,phone&pageSize=10&page=1&apiKey=#{ENV["api_key"]}")
+    raw_stores = JSON.parse(response.body, symbolize_names: true)[:stores]
+    end
 end
