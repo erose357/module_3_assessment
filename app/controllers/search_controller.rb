be3ff1cd080f419711497725a233eedfdf2b9a6f
erose357 <erose357@gmail.com>
2017-11-01 10:02:46 -0600

    Read through api docs and made some test calls through postman and the console, starting to build the search index action

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,8 @@
 class SearchController < ApplicationController
   def index
+    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,longName,city,distance,phone&pageSize=10&page=1&apiKey=qy8vdpfcgsxtfw657mnbu7nc")
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    binding.pry
   end
 end
