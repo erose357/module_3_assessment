a0907a0b2b56823468c153d4e2fd1539162f0863
erose357 <erose357@gmail.com>
2017-11-01 11:32:50 -0600

    Item index and show endpoints complete

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -5,10 +5,33 @@ RSpec.describe "Items API" do
     items = create_list(:item, 5)
 
     get '/api/v1/items'
-#When I send a GET request to `/api/v1/items`
-#I receive a 200 JSON response containing all items
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
     expect(response).to be_success
-#And each item has an id, name, description, and image_url but not the created_at or updated_at
+    expect(json.count).to eq(5)
+    expect(json[0][:id]).to eq(items[0].id)    
+    expect(json[0][:name]).to eq(items[0].name)    
+    expect(json[0][:description]).to eq(items[0].description)    
+    expect(json[0][:image_url]).to eq(items[0].image_url)    
+    expect(json[0][:updated_at]).to be_nil
+    expect(json[0][:created_at]).to be_nil
+  end
+
+  it "sends one items" do
+    item = create(:item)
+
+    get "/api/v1/items/#{item.id}"
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_success
+    expect(json.class).to eq(Hash)
+    expect(json[:name]).to eq(item.name)    
+    expect(json[:description]).to eq(item.description)    
+    expect(json[:image_url]).to eq(item.image_url)    
+    expect(json[:updated_at]).to be_nil
+    expect(json[:created_at]).to be_nil
   end
 end
     
